f09203bfb2ca811ee1ea56767be504cd3e756323
Boyang Yan <yanboyang713@gmail.com>
2022-03-03 00:12:17 +1100

    Published on 2022-03-03 00:12:17

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ deploy () {
     echo "$1"
 
     # Get System environment, if does NOT have get repo name auto
-    if [ -z "${BLOGREPOSITORIENAME}" ]; then
+    if [ ! -z "${BLOGREPOSITORIENAME}" ]; then
         echo "Your Github Repositorir Name is: $BLOGREPOSITORIENAME"
     else
         # get Repositorie Name for deploy SSH key
